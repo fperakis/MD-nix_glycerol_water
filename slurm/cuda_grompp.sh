678ddcbf6ec451beb31a413837525a08c4409a45
Foivos Perakis <fpera@sol-nix.fysik.su.se>
2022-11-25 14:45:07 +0100

    updated cuda_grompp

--- slurm/cuda_grompp.sh
+++ slurm/cuda_grompp.sh
@@ -18,19 +18,23 @@ POSITIONAL_ARGS=()
 while [[ $# -gt 0 ]]; do
   case $1 in
     -f)
-      INPUT_FILE="$2"
+      INPUT="$2"
       shift 
       ;;
     -c)
-      STRUCTURE_FILE="$2"
+      STRUCTURE="$2"
       shift
       ;;
     -t)
       TRAJECTORY="$2"
       shift 
       ;;
+    -p)
+      TOPOLOGY="$2"
+      shift 
+      ;;  
     -o)
-      OUTPUT_FILE="$2"
+      OUTPUT="$2"
       shift 
       ;;
     -*|--*)
@@ -57,6 +61,6 @@ fi
 # to avoid complaints from GROMACS
 export OMP_NUM_THREADS=$ntomp
 
-echo "mpirun gmx_mpi grompp -f ${INPUT_FILE} -c ${STRUCTURE_FILE} -t ${TRAJECTORY} -p topol.top -o ${OUTPUT_FILE}"
+echo "mpirun gmx_mpi grompp -f ${INPUT} -c ${STRUCTURE} -t ${TRAJECTORY} -p ${TOPOLOGY} -o ${OUTPUT}"
 #mpirun gmx_mpi grompp -f $1 -c $2 -t $3 -p topol.top -o $4
 #mpirun gmx_mpi grompp -f mdp/md.mdp -c npt.gro -t npt.cpt -p topol.top -o md_0_1.tpr
