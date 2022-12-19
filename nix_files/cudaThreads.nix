c1035ecaf3b2f891c1e18d2b0dd0d6ea90dc46d6
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2022-12-19 01:20:18 +0100

    update gromacs version and dependecies

--- nix_files/cudaThreads.nix
+++ nix_files/cudaThreads.nix
@@ -27,7 +27,7 @@ let
 in pkgs.mkShell {
   buildInputs = with pkgs; [
     # CUDA MPI version
-    nixGL qchem.gromacsCudaMpi
+    nixGL (qchem.gromacs.override { enableCuda = true; })
   ];
 
   shellHook = ''
